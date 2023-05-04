d8cb8264a5cab6035cd922c3fa9ac5e6505a01e7
Kieran <kieran.webdevelopment@gmail.com>
2023-05-04 12:12:48 +0100

    fetching data successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,56 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import WatchList from './Pages/WatchList';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  useEffect(() => {}, []);
+  const [dataToDisplay, setDataToDisplay] = useState({});
+  // const [imdbIDarray, setImdbIDarray] = useState({});
+
+  useEffect(() => {
+    const apiKey = '10594f76';
+    async function getGeneralFilmInfo() {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&s=star wars&type=movie`
+      );
+      const data = await response.json();
+      const imdbIDarray = data.Search.map((movie) => movie.imdbID);
+      return imdbIDarray;
+    }
+
+    async function getDetailedFilmInfo() {
+      const ids = await getGeneralFilmInfo();
+      console.log(ids);
+      const neededMovieData = await Promise.all(
+        ids.map(async (movieId) => {
+          const response = await fetch(
+            `http://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`
+          );
+          const data = await response.json();
+          // console.log(data);
+          const { Title, imdbRating, Runtime, Genre, Plot, Poster, imdbID } =
+            data;
+          const dataToDisplay = {
+            Title,
+            imdbRating,
+            Runtime,
+            Genre,
+            Plot,
+            Poster,
+            imdbID,
+            wishList: false,
+          };
+          return dataToDisplay;
+        })
+      );
+      setDataToDisplay(neededMovieData);
+      // console.log(neededMovieData);
+    }
+    getDetailedFilmInfo();
+  }, []);
+
+  console.log(dataToDisplay);
+
   return (
     <>
       <Routes>
